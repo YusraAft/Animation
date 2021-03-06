ff6aa0869ad61d51f52859b53b0a664dc8aec536
Yusra <aftabyusra786@gmail.com>
2021-03-06 14:00:52 -0600

    Animation not working

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -15,5 +15,29 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func onPanGesture(_ panGesture:  UIPanGestureRecognizer) {
+        
+        let location = panGesture.location(in: view)
+        print("Location: x: \(location.x), y: \(location.y)")
+        let kartview = panGesture.view!
+        
+        kartview.center = location
+    }
+    
+    //remmeber to change sender
+    @IBAction func didTapGesture(_ didTapGesture: UITapGestureRecognizer) {
+        
+        
+    }
+    
+    
 }
+
+//step
+//drag gesture onto the item
+//Then from th left panel control drag the gesture onto the view controller -> make sure that the type is action nd gesture not any!
+//Next, drag each of the other gestures onto the same function
+//got to the image and click in proerties user interaction enabled
+//now code in what you want when the gesture is used !
+//remmeber rotation is in radians
 
